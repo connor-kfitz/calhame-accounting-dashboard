71e64b0a4119814b56dd74a1a2820c2a9d1438c9
connor-kfitz <connorkfitzsimmons@gmail.com>
2026-02-18 22:10:00 -0500

    Updated Routing in Middleware

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
@@ -11,6 +12,17 @@ const isWebhookRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  const { pathname } = req.nextUrl;
+
+  if (pathname === '/') {
+    if (userId) {
+      return NextResponse.redirect(new URL('/dashboard', req.url));
+    } else {
+      return NextResponse.redirect(new URL('/sign-in', req.url));
+    }
+  }
+
   if (!isPublicRoute(req) && !isWebhookRoute(req)) {
     await auth.protect()
   }
